0ec97bd7eb18f3d5b0d8d56bb755fddd4ed2595d
Roman <dev@rman.dev>
2026-06-18 20:42:10 +0100

    install solidjs, update deps

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,11 @@
+import solid from "@astrojs/solid-js";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "static",
   build: { format: "preserve" },
+  integrations: [solid()],
   vite: { plugins: [tailwindcss()] },
   redirects: { "/github": "https://github.com/dependents-dev/dependents.dev" },
   site: "https://dependents.dev",
